6324a3a956251101a6f6e6d2a0afda6ebaded808
Saleh Hassan <saleh.rasta@gmail.com>
2020-12-01 13:05:24 +0100

    first commit

---
+++ src/main/java/experis/humansvszombies/hvz/HvzApplication.java
@@ -0,0 +1,13 @@
+package experis.humansvszombies.hvz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HvzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HvzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/experis/humansvszombies/hvz/HvzApplicationTests.java
@@ -0,0 +1,13 @@
+package experis.humansvszombies.hvz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HvzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
