d1b83dcd24cba60421ab4e1425e8dc29269203f7
PerChrister <perchrister96@gmail.com>
2020-12-04 14:33:24 +0100

    feat: added react bootstrap

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,14 +4,21 @@ import { clearUser } from "../utils/localstorage.js";
 
 const Home = () => {
 
+  const user = firebaseConfig.auth().currentUser.email;
+  const isVerified = firebaseConfig.auth().currentUser.emailVerified;
+
+  console.log(firebaseConfig.auth().currentUser.emailVerified)
+
   const handleSignOut = () => {
-    firebaseConfig.auth().signOut() 
+    firebaseConfig.auth().signOut()
     clearUser();
   }
 
   return (
     <>
       <h1>Home</h1>
+      <p>Welcome, { user }</p>
+      <p>Verified: { isVerified ? <span>true</span> : <span>false</span>}</p>
       <button onClick={handleSignOut}>Sign out</button>
     </>
   );
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -4,17 +4,23 @@ import { Link } from "react-router-dom";
 import { storeUserDB } from "../utils/dbstorage";
 import firebaseConfig from "../utils/firebase";
 import { storeUser } from "../utils/localstorage";
+import Form from 'react-bootstrap/Form'
 
 const Register = ({ history }) => {
     const handleRegister = useCallback(async event => {
         event.preventDefault();
-        const { email, password } = event.target.elements;
+        const { username, email, password } = event.target.elements;
         try {
-            await firebaseConfig
-                .auth()
-                .createUserWithEmailAndPassword(email.value, password.value);
+            firebaseConfig.auth().createUserWithEmailAndPassword(email.value, password.value)
+                .then(userData => {
+                    userData.user.sendEmailVerification();
+                    console.log(userData);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
             history.push("/")
-            storeUser(email.value);
+            storeUser(username.value);
             storeUserDB(email.value);
         } catch (error) {
             alert(error);
@@ -24,17 +30,40 @@ const Register = ({ history }) => {
     return (
         <div>
             <h1>Register</h1>
-            <form onSubmit={handleRegister}>
-                <label>
-                    Email
-          <input name="email" type="email" placeholder="Email" />
-                </label>
-                <label>
-                    Password
-          <input name="password" type="password" placeholder="Password" />
-                </label>
-                <button type="submit">Register</button>
-            </form>
+            <Form onSubmit={handleRegister}>
+                <Form.Group controlId="formFirstName">
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control name="firstname" type="text" placeholder="First Name" />
+                </Form.Group>
+
+                <Form.Group controlId="formLastName">
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control name="lastname" type="text" placeholder="Last Name" />
+                </Form.Group>
+
+                <Form.Group controlId="formUserName">
+                    <Form.Label>User Name</Form.Label>
+                    <Form.Control name="username" type="text" placeholder="Username" />
+                </Form.Group>
+
+                <Form.Group controlId="formPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control name="password" type="password" placeholder="Password" />
+                </Form.Group>
+
+                <Form.Group controlId="formEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control name="email" type="email" placeholder="Email" />
+                </Form.Group>
+
+                <Form.Group controlId="formAdminCheckbox">
+                    <Form.Check name="admin-checkbox" type="checkbox" label="Register as admin?" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Register
+                </Button>
+            </Form>
             <Link to="/login">Log In</Link>
         </div>
     );
