041a3afc5198ad48eda200e420a33fe0762c87b0
PerChrister <perchrister96@gmail.com>
2020-12-08 09:12:26 +0100

    chore: commented out phone verification code for now

--- frontend/src/components/CurrentGames.js
+++ frontend/src/components/CurrentGames.js
@@ -2,7 +2,7 @@ import React from "react";
 import firebaseConfig from "../utils/firebase";
 import './Components.css';
 import Header from "./Header";
-import PhoneLogin from "./PhoneLogin";
+// import PhoneLogin from "./PhoneLogin";
 
 const CurrentGames = () => {
 
@@ -18,23 +18,28 @@ const CurrentGames = () => {
                         <tr>
                             <th>Game</th>
                             <th>Status</th>
+                            <th></th>
                         </tr>
                         <tr>
                             <td>Cool Game</td>
                             <td>Active</td>
+                            <th><button className="registered">Join</button></th>
                         </tr>
                         <tr>
                             <td>Cooler Game</td>
                             <td>Register</td>
+                            <th><button className="unregistered">Join</button></th>
                         </tr>
                         <tr>
                             <td>Coolest Game</td>
                             <td>Complete</td>
+                            <th><button className="unregistered">Complete</button></th>
                         </tr>
                     </table>
+                    <br></br>
 
                     { isVerified ?
-                        <PhoneLogin />
+                        <p>Phone Login</p>
                         : <p>Verify your email to login with phone.</p>}
                 </div>
             </section>
