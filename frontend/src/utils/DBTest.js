3736aa81f3c3498ad81fd2188be54f01032a6cae
PerChrister <perchrister96@gmail.com>
2020-12-09 10:50:17 +0100

    feat: tested database fetching

--- frontend/src/utils/DBTest.js
+++ frontend/src/utils/DBTest.js
@@ -6,20 +6,14 @@ const DBTest = () => {
     const [users, setUsers] = useState([]);
 
     const getUserInfo = () => {
-        fetch('/api/fetch/useraccount/0')
+        fetch('http://localhost:8080/api/fetch/useraccount/all')
             .then(response => response.json())
             .then(data => setUsers(data));
     }
 
-    console.log(users)
-
     return (
         <>
             <p>Hej</p>
-            { users ?
-                <p>{users.userAccountId}</p>
-                : <p>Error</p>}
-            <button onClick={getUserInfo}>Click me</button>
         </>
     );
 };
