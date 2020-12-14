29547940b4c0b020816b7f7ca12090de631a61b3
Heso113 <hampe_olsson91@hotmail.com>
2020-12-14 08:31:30 +0100

    fix: simple fetch of gamelist from backend

--- frontend/src/components/CurrentGames.js
+++ frontend/src/components/CurrentGames.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import firebaseConfig from "../utils/firebase";
 import './Stylings/Components.css';
 import Header from './Stylings/Header';
@@ -7,6 +7,23 @@ import Header from './Stylings/Header';
 const CurrentGames = () => {
 
     const isVerified = firebaseConfig.auth().currentUser.emailVerified;
+    const [games, setGames] = useState([]);
+
+
+    useEffect(() => {
+        fetchGames();
+    },[])
+
+    async function fetchGames() {
+        const response = await (await fetch('http://localhost:8080/api/fetch/game/all')).json();
+        setGames(response);
+        console.log(response);
+        console.log(games);
+    }
+
+    function handleJoin(id){
+        console.log(id);
+    }
 
     return (
         <>
@@ -20,21 +37,13 @@ const CurrentGames = () => {
                             <th>Status</th>
                             <th></th>
                         </tr>
-                        <tr>
-                            <td>Cool Game</td>
-                            <td>Active</td>
-                            <th><button className="registered">Join</button></th>
-                        </tr>
-                        <tr>
-                            <td>Cooler Game</td>
-                            <td>Register</td>
-                            <th><button className="unregistered">Join</button></th>
-                        </tr>
-                        <tr>
-                            <td>Coolest Game</td>
-                            <td>Complete</td>
-                            <th><button className="unregistered">Complete</button></th>
-                        </tr>
+                        {games.map((g) =>
+                            <tr>
+                                <td>{g.name}</td>
+                                <td>{g.gameState}</td>
+                                <td><button type="button" onClick={() => handleJoin(g.gameId)}>Join</button></td>
+                            </tr>
+                        )}
                     </table>
                     <br></br>
 
