5a7efd7f6b062bd9059b04b7874270b5930d8d8c
Heso113 <hampe_olsson91@hotmail.com>
2020-12-17 10:31:23 +0100

    feat: now saves GameId, PlayerId and SquadId to localStorage when a user joins a game.

--- frontend/src/components/CurrentGames.js
+++ frontend/src/components/CurrentGames.js
@@ -20,11 +20,81 @@ const CurrentGames = (props) => {
     }
 
     function handleJoin(id) {
-        localStorage.setItem("gameId", id);
-        props.history.push("/");
+        localStorage.setItem("Game ID", id);
+        loadPlayerInformationToLocalStorage(id);
+        props.history.push("/landing");
         console.log(id);
     }
 
+    async function loadPlayerInformationToLocalStorage(gameId) {
+        let userId = localStorage.getItem("User ID");
+        let status = await fetchPlayerObjectFromDB(gameId);
+        if (status === true) {
+            console.log("User already had a Player.");
+        } else {
+            console.log("User did not have a Player object. Attempting to create one.");
+            status = await createPlayerObject(gameId, userId);
+            if(status === true) {
+                console.log("Player object created successfully.");
+            } else {
+                console.log("Failed to create Player Object.");
+            }
+        }
+        let playerId = localStorage.getItem("Player ID");
+        status = await fetchSquadFromDB(gameId, playerId);
+        if (status === true) {
+            console.log("Player belongs to a Squad.")
+        } else {
+            console.log("Player does not belong to a Squad.");
+        }  
+    }
+
+    async function fetchPlayerObjectFromDB(gameId) {
+        let userId = localStorage.getItem("User ID");
+        let response = await fetch("http://localhost:8080/api/fetch/player/game=" + gameId + "/user=" + userId);
+        let status = await response.status;
+        if (status === 200) {
+            let body = await response.json();
+            localStorage.setItem("Player ID", body.playerId);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    async function createPlayerObject(gameId, userId) {
+        let response = await fetch("http://localhost:8080/api/create/player/" + userId + "/" + gameId, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    playerId: null,
+                    faction: 'HUMAN',
+                    alive: true,
+                    patientZero: false
+                })
+            });
+            if (response.status === 201) {
+                let body = await response.json();
+                localStorage.setItem("Player ID", body.playerId);
+                return true;
+            } else {
+                return false;
+            }
+    }
+
+    async function fetchSquadFromDB(gameId, playerId,) {
+        let response = await fetch("http://localhost:8080/api/fetch/squad/game=" + gameId + "/player=" + playerId);
+        if (response.status === 200) {
+            let body = await response.json();
+            localStorage.setItem("Squad ID", body.squadId);
+            return true;
+        } else {
+            localStorage.setItem("Squad ID", null);
+            return false;
+        }   
+    }
 
     return (
         <>
