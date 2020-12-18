bcab50037b5483dedece8a8abf5ce4e7adcd0d9d
Heso113 <hampe_olsson91@hotmail.com>
2020-12-18 10:13:47 +0100

    Update setupProxy.js

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,6 +1,6 @@
 const { createProxyMiddleware }  = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(createProxyMiddleware('/api', 
-        { target: 'https://localhost:8080/' }
+        { target: 'http://localhost:8080/' }
     ));
 }
