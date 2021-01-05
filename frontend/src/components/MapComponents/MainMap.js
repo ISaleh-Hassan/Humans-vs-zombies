22cee33bb46c258f4165145ffffb82a12fc3fafd
PerChrister <perchrister96@gmail.com>
2021-01-05 15:27:48 +0100

    fix: added css files to map

--- frontend/src/components/MapComponents/MainMap.js
+++ frontend/src/components/MapComponents/MainMap.js
@@ -154,8 +154,8 @@ class MainMap extends Component {
   render() {
     return (
       <>
-        <link href="https://api.mapbox.com/mapbox-gl-js/v2.0.0/mapbox-gl.css" rel="stylesheet" />
-        <script src='https://api.mapbox.com/mapbox-gl-js/v2.0.0/mapbox-gl.js'></script>
+        <link href="https://api.mapbox.com/mapbox-gl-js/v2.0.1/mapbox-gl.css" rel="stylesheet" />
+        <script src='https://api.mapbox.com/mapbox-gl-js/v2.0.1/mapbox-gl.js'></script>
         <div ref={el => this.mapContainer = el} className='leaflet-container'></div>
         <label>Marker Location: </label>
         <p id="coordinates"></p>
